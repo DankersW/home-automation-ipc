68b7f5fe4c6c30094cbaf1ebb60ee6b721496b86
DankersW <wouter.dankers@skynet.be>
2021-11-04 16:50:20 +0100

    toolchain: running

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,7 +13,6 @@ docker run \
     --rm \
 	--user $UID:$UID \
 	--volume ${BASE_DIR}:/work \
-    -e XDG_CACHE_HOME=/tmp/.cache \
 	-it --privileged \
     ${IMAGE_NAME}:${IMAGE_TAG} $@ \
         || fail "running docker, better luck next time"
