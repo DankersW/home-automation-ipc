aa2e0651f7d8573288eb2ade33be5870190f0312
DankersW <wouter.dankers@skynet.be>
2021-12-10 17:00:06 +0100

    toolchain: generating nano pb files

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-docker login
-
 mkdir -p generated/go
 mkdir -p generated/c
+mkdir -p generated/nanopb
+
+for file in "pb_common.c" "pb_common.h" "pb_decode.c" "pb_decode.h" "pb_encode.c" "pb_encode.h" "pb.h"; do
+    cp nanopb/$file generated/nanopb
+done
 
 proto_files=("wsn.proto")
-
-for val1 in ${proto_files[*]}; do
-    echo "Generating proto file: $val1"
-    ./scripts/docker_run.sh --c_out=generated/c --go_out=generated/go -I. $val1
+for proto_file in ${proto_files[*]}; do
+    echo "Generating proto file: $proto_file"
+    ./scripts/docker_run.sh --c_out=generated/c --go_out=generated/go -I. $proto_file
+    nanopb/generator/protoc --nanopb_out=generated/nanopb $proto_file
 done
