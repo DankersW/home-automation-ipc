2438d994492d2faa54490a5b547b4e290bbc543f
DankersW <wouter.dankers@skynet.be>
2022-03-01 15:28:48 +0100

    proto: defined wsn telemetry data

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -8,7 +8,7 @@ for file in "pb_common.c" "pb_common.h" "pb_decode.c" "pb_decode.h" "pb_encode.c
     cp nanopb/$file generated/nanopb
 done
 
-proto_files=("wsn.proto")
+proto_files=("wsn.proto", "ipc.proto")
 for proto_file in ${proto_files[*]}; do
     echo "Generating proto file: $proto_file"
     ./scripts/docker_run.sh --c_out=generated/c --go_out=generated/go -I. $proto_file
